2a82503bb87a7e3b5f09c8a52ca473c17bafb87c
Anton Kalochelitis <fire.anton@gmail.com>
2022-09-13 00:46:31 +0300

    update composer.json, add ENV config file

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@core', dirname(__DIR__));
+Yii::setAlias('@core', dirname(__DIR__) . '/core');
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@temp', dirname(__DIR__));
+Yii::setAlias('@temp', dirname(__DIR__) . '/temp');
